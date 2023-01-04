d08b6d96c9bdff61acc14dadc628767b232409be
Alusp <alusinejalloh800@gmail.com>
2023-01-05 01:02:06 +0200

    Add Redux files

---
+++ src/redux/greeting.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const GREETINGS = 'GREETINGS';
+
+const init = [];
+
+const greetingReducer = (state = init, action) => {
+  switch (action.type) {
+    case GREETINGS:
+      return action.greetings;
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
+
+const url = 'http://localhost:3000/api/v1/greetings';
+
+export const getGreetings = createAsyncThunk(GREETINGS, async (args, { dispatch }) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const { data } = await axios.get(url, config);
+    dispatch({ type: GREETINGS, greetings: data[0] });
+  } catch (err) {
+  // display error messages
+  }
+});
